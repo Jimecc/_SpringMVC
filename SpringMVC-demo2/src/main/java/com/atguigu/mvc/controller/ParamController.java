c6ea15a94fc350feff9082cca05f4c5986d9ff5a
Jimecc <1813085460@qq.com>
2022-08-06 01:19:13 +0800

    NotInt

---
+++ SpringMVC-demo2/src/main/java/com/atguigu/mvc/controller/ParamController.java
@@ -0,0 +1,18 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Jim
+ * @Description
+ * @createTime 2022年08月06日
+ */
+
+@Controller
+public class ParamController {
+
+
+
+}
--- SpringMVC-demo2/src/main/java/com/atguigu/mvc/controller/RequestMappingController.java
+++ SpringMVC-demo2/src/main/java/com/atguigu/mvc/controller/RequestMappingController.java
@@ -1,6 +1,8 @@
 package com.atguigu.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,4 +23,47 @@ public class RequestMappingController {
     public String success(){
         return "success";
     }
+
+    @GetMapping("/testGetMapping")
+    public String testGetMapping(){
+        return "success";
+    }
+    @GetMapping(value = "/testParams",params = {"username","password"})
+    public String testParams(){
+        return "success";
+    }
+    @GetMapping(value = "/testHeaders",headers = {"Host=localhost:8081"})
+    public String testHeaders(){
+        return "success";
+    }
+
+    @RequestMapping("/testAnt")
+    public String testAnt(){
+        return "success";
+    }
+    @RequestMapping("/a?a/testAnt")
+    public String testAnt2(){
+        return "success";
+    }
+
+    @RequestMapping("/**/testXX")
+    public String testAnt3(){
+        return "xx";
+    }
+//    @GetMapping("/a*a/testAnt")
+//    public String testAnt4GetMapping(){
+//        return "success";
+//    }
+
+    @RequestMapping("/{id}/testPath")
+    public String testPathVariable(@PathVariable Integer id){
+        System.out.println("id:"+id);
+        return "pathVariable";
+    }
+
+    @RequestMapping("/param")
+    public String testParam(){
+        return "test_Param";
+    }
+
 }
