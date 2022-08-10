cf9fed026ecd9a9e882de04d2a57fc1755cbeaad
Jimecc <1813085460@qq.com>
2022-08-10 21:57:08 +0800

    ExceptionHandler

--- SpringMVC-demo5/src/main/java/com/atguigu/mvc/interceptors/SecondInterceptor.java
+++ SpringMVC-demo5/src/main/java/com/atguigu/mvc/interceptors/SecondInterceptor.java
@@ -3,11 +3,9 @@ package com.atguigu.mvc.interceptors;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 /**
  * @author Jim
@@ -16,11 +14,10 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 @Component
-//public class FirstInterceptor extends HandlerInterceptorAdapter {
-public class FirstInterceptor  implements HandlerInterceptor {
+public class SecondInterceptor  implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("FirstInterceptor-----preHandle");
+        System.out.println("SecondInterceptor-----preHandle");
         return HandlerInterceptor.super.preHandle(request, response, handler);
 //        return false;
 
@@ -28,15 +25,14 @@ public class FirstInterceptor  implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("FirstInterceptor-----postHandle");
+        System.out.println("SecondInterceptor-----postHandle");
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("FirstInterceptor-----afterCompletion");
+        System.out.println("SecondInterceptor-----afterCompletion");
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
-
 
 }
